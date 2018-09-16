87f981efdba0f1416850b972b1543bf2a3b3400a
Jayesh Jain <jjayesh@fb.com>
2018-09-16 15:59:55 -0700

    Change text

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express().use(bodyParser.json());
-app.get('/', (req, res) => res.send('Welcome to Jayesh messenger bot webhook on heroku!'))
+//app.get('/', (req, res) => res.send('Welcome to Jayesh messenger bot webhook on heroku!'))
+app.get('/', (req, res) => res.send('271724301'));
 var port = process.env.PORT || 8080;
 app.listen(port, () => console.log('Example app listening on port %d', port));
 
